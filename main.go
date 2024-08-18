e799c1f33a5b89f05f3edb9f100d15066fecdeaa
Arup Chauhan <arupchauhan26@gmail.com>
2024-08-17 22:31:39 -0500

    Added GO module and web server
    Signed-off-by: Arup Chauhan <arupchauhan26@gmail.com>

---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Welcome to the Webserver: Level 1")
+	})
+
+	fmt.Println("Server running on standard 8080 localhost port")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+}
